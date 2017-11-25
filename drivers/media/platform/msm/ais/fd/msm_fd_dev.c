d5ad29f9ffcbdc296bc0ba470c6ba5f6c553e04a
Indrasena Reddy G <gisena@codeaurora.org>
2017-11-25 14:03:14 +0530

    msm: camera: Make use of mutex lock to avoid race condition
    Make use of mutex lock to access IOCTL so that two threads
    can avoid race condition.
    
    Change-Id: If3ef9a3eaeaf24a89c4b277b1ca944f6a3a9f021

--- drivers/media/platform/msm/ais/fd/msm_fd_dev.c
+++ drivers/media/platform/msm/ais/fd/msm_fd_dev.c
@@ -223,6 +223,8 @@ static void msm_fd_buf_queue(struct vb2_buffer *vb)
 
 	if (vb->vb2_queue->streaming)
 		msm_fd_hw_schedule_and_start(ctx->fd_device);
+
+	return;
 }
 
 /*
@@ -1053,14 +1055,18 @@ static int msm_fd_s_ctrl(struct file *file, void *fh, struct v4l2_control *a)
 			a->value = ctx->format.size->work_size;
 		break;
 	case V4L2_CID_FD_WORK_MEMORY_FD:
-		if (ctx->work_buf.fd != -1)
-			msm_fd_hw_unmap_buffer(&ctx->work_buf);
+                mutex_lock(&ctx->fd_device->recovery_lock);
+                if (ctx->work_buf.fd != -1)
+                        msm_fd_hw_unmap_buffer(&ctx->work_buf);
 		if (a->value >= 0) {
 			ret = msm_fd_hw_map_buffer(&ctx->mem_pool,
 				a->value, &ctx->work_buf);
-			if (ret < 0)
+			if (ret < 0) {
+				mutex_unlock(&ctx->fd_device->recovery_lock);
 				return ret;
+			}
 		}
+		mutex_unlock(&ctx->fd_device->recovery_lock);
 		break;
 	default:
 		return -EINVAL;
@@ -1070,7 +1076,7 @@ static int msm_fd_s_ctrl(struct file *file, void *fh, struct v4l2_control *a)
 }
 
 /*
- * msm_fd_cropcap - V4l2 ioctl crop capabilities.
+ * msm_fd_cropcap - V4l2 ioctl crop capabilites.
  * @file: Pointer to file struct.
  * @fh: V4l2 File handle.
  * @sub: Pointer to v4l2_cropcap struct need to be set.
